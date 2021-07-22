cea2653bb13977336cad7d6b9cae21a5dba0ecbf
Cezar Augusto <boss@cezaraugusto.net>
2021-07-22 10:37:12 -0300

    Live

---
+++ js/main.js
@@ -0,0 +1,36 @@
+console.log(
+  '%cTá perdido??? %cdigite hacker()',
+  `
+    color: magenta;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 6rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  `,
+  `
+    color: rebeccapurple;
+    font-weight: bold;
+    font-size: 2rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  `
+);
+
+function hacker() {
+  console.log(
+    '%cAgora cole este código:\n%cwindow.atob("aHR0cHM6Ly94dmlkZW9zLmNvbQ==")',
+    `
+      color: purple;
+      font-weight: bold;
+      font-size: 2rem;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    `,
+    `
+    margin-top: 10px;
+      color: gold;
+      font-weight: bold;
+      text-transform: uppercase;
+      font-size: 1rem;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    `
+  );
+}
